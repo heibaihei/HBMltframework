5ae09134f0e0aa7be4a6b18f7ac0fd91d49b8195
黄存璘 <hcl@meitu.com>
2017-09-30 11:00:42 +0800

    [工程更新] 提交可用mlt 编译

---
+++ proj/mac/HBMltframework/MltframeworkSrc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MltframeworkSrc
+//
+//  Created by zj-db0519 on 2017/9/29.
+//  Copyright © 2017年 meitu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ thirdparty/mlt-6.4.0/compile_mlt.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+MLT_PROJ_ROOT_PATH=$(pwd)
+MLT_LIB_OUTPUR_PATH=${MLT_PROJ_ROOT_PATH}/../../lib/mltframework
+
+cd ${MLT_PROJ_ROOT_PATH} \
+&& ./configure --prefix="${MLT_LIB_OUTPUR_PATH}" --enable-debug --enable-gpl --enable-gpl3 
